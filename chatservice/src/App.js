123bade404ad1b1128510dc87f586b80a3bd9b75
Caroleannv <43645688+Caroleannv@users.noreply.github.com>
2022-01-21 21:00:41 -0500

    Communication with firebase DB

--- chatservice/src/App.js
+++ chatservice/src/App.js
@@ -6,6 +6,7 @@ import 'firebase/compat/firestore';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 firebase.initializeApp({
   apiKey: "AIzaSyDTBptzFEuH8iW8Up5uKUYYlZX86v4DTKU",
@@ -24,7 +25,8 @@ function App() {
   const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+      <SignOut />
 
       <section>
         {user ? <ChatRoom /> : <SignIn />}
@@ -55,26 +57,55 @@ function ChatRoom() {
   const query = message.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, {idField: 'id'});
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    //Disable page refresh on the event
+    e.preventDefault();
+    const {uid, photoURL} = auth.currentUser;
+    //Write new document to firestore
+    await message.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid, 
+      photoURL 
+    });
+    //Reset formValue
+    setFormValue('');
+  }
 
   return (
+    <>
       <div>
         {messages && messages.map(msg => 
           <ChatMessage key={msg.id} message={msg} />)}
       </div>
-  )
+
+      <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+
+      <button type="submit">Test</button>
+      
+      </form>
+    </>
+    );
 }
 
+
+
 function ChatMessage(props) { 
-  const { text, uid } = props.message;
+  const { text, uid, photoURL } = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  
   return (
-    <div className={`message ${messageClass}`}>
-      <p>{text}</p>
-    </div>
-  )
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL}/>
+        <p>{text}</p>
+      </div>
+  </>)
 }
 
 export default App;
